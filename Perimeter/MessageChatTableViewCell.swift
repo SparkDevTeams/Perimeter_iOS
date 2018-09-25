a3e239ac42039e91467a0bb7328ff6b5b4146ab3
Ashley <ashleyrennee@Ashleys-MacBook-Air.local>
2018-09-25 13:09:23 -0400

    changes

---
+++ Perimeter/MessageChatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageChatTableViewCell.swift
+//  Perimeter
+//
+//  Created by Ashley on 9/20/18.
+//  Copyright © 2018 SparkDev. All rights reserved.
+//
+
+import UIKit
+
+class MessageChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
