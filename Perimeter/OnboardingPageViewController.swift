9ae099b5fb2a171dd79225bc7db8249627ac14c0
Joshua Martinez <joshuamartinez@hurv001.ad.fiu.edu>
2018-10-09 00:54:05 -0400

    Everything but Link from "get started" button to signup page. Deleted white space

--- Perimeter/OnboardingPageViewController.swift
+++ Perimeter/OnboardingPageViewController.swift
@@ -12,53 +12,35 @@ import UIKit
 class OnboardingPageViewController: UIPageViewController, UIPageViewControllerDataSource {
     
     lazy var viewControllerList: [UIViewController] = {
-        
         let sb = UIStoryboard(name: "Onboarding", bundle: nil)
-        
         let vc1 = sb.instantiateViewController(withIdentifier: "OneVC")
         let vc2 = sb.instantiateViewController(withIdentifier: "TwoVC")
         let vc3 = sb.instantiateViewController(withIdentifier: "ThreeVC")
-        
         return [vc1, vc2, vc3]
     }()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.dataSource = self
-        
         if let firstViewController = viewControllerList.first {
             self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        
         guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
-        
         let previousIndex = vcIndex - 1
-        
         guard previousIndex >= 0 else {return nil}
-        
         guard viewControllerList.count > previousIndex else {return nil}
-        
         return viewControllerList[previousIndex]
-        
         // create an array of objects that take 3 items, 2 text labels, and 1 image.
-        
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        
         guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
-        
         let nextIndex = vcIndex + 1
-        
         guard viewControllerList.count != nextIndex else {return nil}
-        
         guard viewControllerList.count > nextIndex else {return nil}
-        
         return viewControllerList[nextIndex]
     }
     
