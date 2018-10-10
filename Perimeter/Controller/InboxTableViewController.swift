ca2db349e236395a485e7713fd2615197e719fb3
Davone Barrett <dbarr095@fiu.edu>
2018-10-10 09:25:24 -0400

    Duplicated datasource conflict

--- Perimeter/Controller/InboxTableViewController.swift
+++ Perimeter/Controller/InboxTableViewController.swift
@@ -31,7 +31,6 @@ extension InboxTableViewController {
         return chatrooms.count
     }
     
-<<<<<<< HEAD
     override func numberOfSections(in tableView: UITableView) -> Int{
         return 1
     }
@@ -41,16 +40,3 @@ extension InboxTableViewController {
         return cell
     }
     
-=======
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
-        
-        return cell
-    }
->>>>>>> a88fced31fec050739d9bb4eef23ca2aff3a326c
-}
