7c66bb838b4204f88d9b42273224007763e50279
Ashley Acevedo <ashleyrenneeacevedo@hotmail.com>
2018-10-25 15:03:03 -0400

    Commit AppDelegate

--- Perimeter/AppDelegate.swift
+++ Perimeter/AppDelegate.swift
@@ -24,9 +24,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
            
             /* configure tabbar bar view, if you want to change the entry point of the app
              comment out configureTabBarView and instantiate your own view and set it as the rootView*/
-             showMain()
+             //showMain()
             //showSignup()
-            //showOnboarding()
+            showOnboarding()
 
             return true
         }
